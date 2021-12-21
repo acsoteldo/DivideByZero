1981fe2f386811b49ce5f5a86fe4d4808069ef6b
Andrea Soteldo <76544489+acsoteldo@users.noreply.github.com>
2021-12-21 17:29:46 -0600

    Add files via upload

---
+++ DivideByZeroException.java
@@ -0,0 +1,13 @@
+package college;
+
+public class DivideByZeroException extends Exception
+{
+	public DivideByZeroException()
+	{
+		super("Dividing by Zero!");
+	}
+	public DivideByZeroException(String message)
+	{
+		super(message);
+	}
+}
